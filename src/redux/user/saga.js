bac3a8f5cb71d46d46c7b8c0c2f43b35ae48eacf
Daniel Felix <felixdaniel9@gmail.com>
2023-06-29 14:57:19 -0300

    Add takeLatest in saga.js

--- src/redux/user/saga.js
+++ src/redux/user/saga.js
@@ -1,12 +1,13 @@
-import { all, takeEvery, call, put, delay } from 'redux-saga/effects'
+import { all, takeEvery, call, put, delay, takeLatest } from 'redux-saga/effects'
 import { fetchUsersSuccess, fetchUsersFailure } from './slice'
 
 import axios from 'axios'
 // API USERS: http://jsonplaceholder.typicode.com/users/")
 
+// Função com asterisco são chamadas de funções geradoras, são usadas quando precisa buscar dados externos como de uma API ou banco de dados, são como async/await(yield = await) 
 function* fetchUsers() {
   try {
-    // Aumenta em 2segundos a demora pra buscar os dados na API, dica de simulação.
+    // Aumenta em 2 segundos a demora pra buscar os dados na API, dica de simulação.
     yield delay(1000)
 
     const response = yield call(axios.get, "http://jsonplaceholder.typicode.com/users/")
@@ -18,5 +19,8 @@ function* fetchUsers() {
 }
 
 export default all([
+  // TakeLatest mostra os dados da ùltima chamada.
+  //takeLatest("user/fetchUsers", fetchUsers),
+  // TakeEvery mostra os dados de todas as chamadas.
   takeEvery("user/fetchUsers", fetchUsers)
 ])
